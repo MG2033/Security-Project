53171436ae8ee32fccc6bbae451ac15ec2a95265
Mostafa Gamal <mostafa.gamal8086@gmail.com>
2018-04-21 13:57:50 +0200

    CA Creation

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from utils import generate_safe_prime, generate_sha_hash
 from elgamal import ElGamalDS
 import random
 
+# Global Constants
 NBYTES = SHA.digest_size + 1
 BYTES_TO_BITS = 8
+MAX_ID = 10
+
+# Message to be sent by Alice
 M = 23
 
 # Alice and Bob Parameters
@@ -14,18 +18,20 @@ a = random.randint(2, q - 1)
 # Alice Keys
 x_a = random.randint(2, q - 1)
 y_a = pow(a, x_a, q)
-id_a = 1
+id_a = random.randint(1, MAX_ID)
 
 # Bob Keys
 x_b = random.randint(2, q - 1)
 y_b = pow(a, x_b, q)
-id_b = 2
+id_b = random.randint(1, MAX_ID)
+
+# ID-A should be different than ID-B
+assert id_a != id_b
 
 # CA Creation
 
 # Message Hashing
 m = generate_sha_hash(M)
-
 
 ##############################################33
 elgamal = ElGamalDS()
