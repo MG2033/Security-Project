a3f355831ceb5d0dcd191b8f174056a654197236
Mostafa Gamal <mostafa.gamal8086@gmail.com>
2018-04-21 13:55:22 +0200

    CA Creation

--- elgamal.py
+++ elgamal.py
@@ -1,9 +1,10 @@
 import random
 import math
+import gmpy
 
 
 class ElGamalDS:
-    def generate(self, x_a, a, q, m):
+    def generate(self, x, a, q, m):
         """Used to generate a digital signature for a message m using ElGamal Digital Signature Algorithm"""
         # Step 1: Make sure that hash of M satisfies the following condition
         assert 0 <= m <= (q - 1)
@@ -13,6 +14,19 @@ class ElGamalDS:
         while math.gcd(K, q - 1) != 1:
             K = random.randint(1, q - 1)
 
-    def verify(self):
+        # Step 3: Calculate S1
+        S1 = pow(a, K, q)
+
+        # Step 4: Calculate inverse of K mod q-1
+        Kinv = gmpy.invert(K, q - 1)
+
+        # Step 5: Calculate S2
+        S2 = (Kinv * (m - x * S1)) % (q - 1)
+
+        return S1, S2
+
+    def verify(self, y, a, m, q, S1, S2):
         """Used to verify a digital signature for a message m using ElGamal Digital Signature Algorithm"""
-        pass
+        V1 = pow(a, m, q)
+        V2 = (pow(y, S1, q) * pow(S1, S2, q)) % q
+        return V1 == V2
--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 from Crypto.Hash import SHA
-from utils import generate_safe_prime
+from utils import generate_safe_prime, generate_sha_hash
 from elgamal import ElGamalDS
+import random
 
 NBYTES = SHA.digest_size + 1
 BYTES_TO_BITS = 8
+M = 23
 
-q = generate_safe_prime(NBYTES)
+# Alice and Bob Parameters
+q = generate_safe_prime(NBYTES * BYTES_TO_BITS)
+a = random.randint(2, q - 1)
 
+# Alice Keys
+x_a = random.randint(2, q - 1)
+y_a = pow(a, x_a, q)
+id_a = 1
+
+# Bob Keys
+x_b = random.randint(2, q - 1)
+y_b = pow(a, x_b, q)
+id_b = 2
+
+# CA Creation
+
+# Message Hashing
+m = generate_sha_hash(M)
+
+
+##############################################33
 elgamal = ElGamalDS()
-elgamal.generate(0, 0, q, 6)
+S1, S2 = elgamal.generate(x_a, a, q, m)
+if elgamal.verify(y_a, a, m, q, S1, S2):
+    print("Signature matches!")
+else:
+    print("Signature doesn't match!")
